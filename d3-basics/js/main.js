bbb9beea6282bfe99f2f00ea32f12268bca4893f
Binu42 <kbinu42@gmail.com>
2020-12-03 01:28:07 +0530

    multiple circles

--- d3-basics/js/main.js
+++ d3-basics/js/main.js
@@ -1,9 +1,20 @@
-var svg = d3.select("#chart-area").append("svg")
+const data = [25, 20, 35, 15];
+
+const svg = d3.select("#chart-area").append("svg")
 	.attr("width", 400)
 	.attr("height", 400);
 
-var circle = svg.append("circle")
-	.attr("cx", 100)
-	.attr("cy", 250)
-	.attr("r", 70)
+const circles = svg.selectAll("circles").data(data);
+
+circles.enter()
+	.append("circle")
+	.attr("cx", (d, i) => {
+		return (i * 50) + 25;
+	})
+	.attr("cy", (d, i) => {
+		return 25;
+	})
+	.attr("r", (d, i) => {
+		return d;
+	})
 	.attr("fill", "blue");
