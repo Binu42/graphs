492b893fc42cd5cc62a9449a511cf99b923a7f4f
Binu42 <kbinu42@gmail.com>
2020-12-06 03:15:12 +0530

    fix: plot issues

--- d3-basics/js/main.js
+++ d3-basics/js/main.js
@@ -1,20 +1,53 @@
-const data = [25, 20, 35, 15];
+// d3.json("data/ages.json").then((data) => {
+// 	data.forEach(data => data.age = +data.age);
+// 	const svg = d3.select("#chart-area").append("svg")
+// 		.attr("width", 400)
+// 		.attr("height", 400);
+// 	console.log(data);
 
-const svg = d3.select("#chart-area").append("svg")
-	.attr("width", 400)
-	.attr("height", 400);
+// 	const circles = svg.selectAll("circles").data(data);
 
-const circles = svg.selectAll("circles").data(data);
+// 	circles.enter()
+// 		.append("circle")
+// 		.attr("cx", (d, i) => {
+// 			return (i * 50) + 25;
+// 		})
+// 		.attr("cy", (d, i) => {
+// 			return 25;
+// 		})
+// 		.attr("r", (d, i) => {
+// 			return d.age * 2;
+// 		})
+// 		.attr("fill", (data) => {
+// 			if (data.name === "Emily")
+// 				return 'pink';
+// 			return 'red';
+// 		});
+// })
 
-circles.enter()
-	.append("circle")
-	.attr("cx", (d, i) => {
-		return (i * 50) + 25;
-	})
-	.attr("cy", (d, i) => {
-		return 25;
-	})
-	.attr("r", (d, i) => {
-		return d;
-	})
-	.attr("fill", "blue");
+d3.json("data/buildings.json").then((data) => {
+	data.forEach(data => data.height = +data.height);
+	const svg = d3.select("#chart-area").append("svg")
+		.attr("width", 400)
+		.attr("height", 400);
+
+	const y = d3.scaleLinear().domain([0, 850]).range([0, 400]);
+
+	const rectangles = svg.selectAll("rect").data(data);
+
+	rectangles.enter()
+		.append("rect")
+		.attr("x", (d, i) => {
+			return (i * 60);
+		})
+		.attr("y", 20)
+		.attr("height", (d, i) => {
+			return y(d.height);
+		})
+		.attr("width", (d, i) => {
+			return 40;
+		})
+		.attr("fill", (data) => {
+			return 'black';
+		});
+})
